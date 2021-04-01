01d32b197380742f807f8159960031a4b7ff8f44
abishek06 <abishekk06@gmail.com>
2021-04-01 16:34:50 +0530

    Adding Expense and Budget Calculation

--- js/app.js
+++ js/app.js
@@ -23,22 +23,88 @@ class UI{
     this.expenseAmount = document.getElementById('expense-amount');
     this.balanceAmount = document.getElementById('balance-amount');
 
+    this.expenseList = document.getElementById('expense-list');
     //to input Id and Object creation
     this.itemId=0;
     this.item={};
+    this.itemArray=[];
+    
   }
 
   
   budgetSubmitForm(){    
-    var enteredBudgetText = this.budgetInput.value;  
-
+    var enteredBudgetText = this.budgetInput.value;      
     if(enteredBudgetText<=0){
       this.budgetFeedback.classList.add('showItem');
       this.budgetFeedback.innerHTML = `<p>The value cannot be negative or zero</p>`;
+      setTimeout(() => {
+        this.budgetFeedback.classList.remove('showItem');
+        this.budgetFeedback.innerHTML = '';
+      }, 3000);     
     }
-
+    else    
+      this.budgetAmount.textContent = enteredBudgetText;
+    
+      this.budgetInput.value='';
   }
 
+  expenseSubmitForm(){
+      let expenseText =  this.amountInput.value;
+      if(expenseText<=0){
+        this.expenseFeedback.classList.add('showItem');
+        this.expenseFeedback.innerHTML=`<p>The value cannot be negative or zero</p>`;
+        setTimeout(() => {
+          this.expenseFeedback.classList.remove('showItem');
+        }, 3000);
+      }
+      else{
+        if(this.expenseAmount.textContent==0)
+            this.expenseAmount.textContent=expenseText;
+        else
+            this.expenseAmount.textContent= parseInt(expenseText)+ parseInt(this.expenseAmount.textContent);
+      }
+      this.amountInput.value='';
+
+
+      
+      this.item={
+        id : this.itemId,
+        title : this.expenseInput.value,
+        amount : expenseText,
+      }
+      this.itemId++;
+      this.itemArray.push(this.item);
+
+      this.bindExpenseList(this.itemArray);
+        
+  }
+
+     bindExpenseList(itemList) {
+       console.log(itemList);
+      
+       itemList.forEach(item => {
+          const div = document.createElement('div');
+         div.classList.add('expense');
+   
+         div.innerHTML = `<div class="expense-item d-flex justify-content-between align-items-baseline">
+     
+         <h6 class="expense-title mb-0 text-uppercase list-item"> ${item.title}</h6>
+         <h5 class="expense-amount mb-0 list-item">$${item.amount}</h5>
+     
+         <div class="expense-icons list-item">
+     
+          <a href="#" class="edit-icon mx-2" data-id="${item.id}">
+           <i class="fas fa-edit"></i>
+          </a>
+          <a href="#" class="delete-icon" data-id="${item.id}">
+           <i class="fas fa-trash"></i>
+          </a>
+         </div>
+        </div`;
+        this.expenseList.appendChild(div);
+       });
+      
+  }
   
 }
 
@@ -53,11 +119,16 @@ function EventListeners(){
   console.log('Event Listeners Called');
   var budgetForm = document.getElementById('budget-form');
   var expenseForm = document.getElementById('expense-form');
-  var expenseList = document.getElementById('expense-list');
+  
 
   budgetForm.addEventListener('submit',function(event){
     event.preventDefault();
     ui.budgetSubmitForm();
+  });
+
+  expenseForm.addEventListener('submit', function(event){
+    event.preventDefault();
+    ui.expenseSubmitForm();
   });
 }
 
